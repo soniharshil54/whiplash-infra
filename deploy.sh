e643062ce51e310c95e626ec57fa9c01794a8098
soniharshil54 <sharshil43@yahoo.com>
2025-10-03 21:54:42 +0530

    add deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,31 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# ──────────────── CONFIG ────────────────
-: "${AWS_REGION:=us-east-1}"
-: "${STAGE:=dev}"
-export PROJECT="whiplash"
-export AWS_PROFILE="soni-1214"
+# load env variables from .env file if it exists
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+fi
+
+echo "Using environment variables:"
+echo "  AWS_REGION: ${AWS_REGION}"
+echo "  DEPLOY_ENV: ${DEPLOY_ENV}"
+echo "  AWS_PROFILE: ${AWS_PROFILE}"
+echo "  PROJECT: ${PROJECT}"
 
 # ──────────────── VERSION & IMAGE ────────────────
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
-VERSION=$(cat VERSION)
+echo "🆔 AWS Account ID: ${AWS_ACCOUNT_ID}"
+echo "📂 Infra DEPLOY_ENV: ${DEPLOY_ENV}"
+export VERSION=$(cat VERSION)
 
 echo "🚀 Deploying infra version: ${VERSION}"
+
+# cdk bootstrap --context stage="${DEPLOY_ENV}" aws://${AWS_ACCOUNT_ID}/${AWS_REGION}
 
 cdk context --clear
 cdk deploy \
   --require-approval never \
-  --context stage="${STAGE}" \
+  --context stage="${DEPLOY_ENV}" \
   --context version="${VERSION}"
 
 echo "✅ Infra ${VERSION} deployed successfully"
--- destroy.sh
+++ destroy.sh
@@ -1,24 +1,29 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# ──────────────── CONFIG ────────────────
-: "${AWS_REGION:=us-east-1}"
-: "${STAGE:=dev}"
-PROJECT="whiplash"
-AWS_PROFILE="soni-1214"
+# load env variables from .env file if it exists
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+fi
+
+echo "Using environment variables:"
+echo "  AWS_REGION: ${AWS_REGION}"
+echo "  DEPLOY_ENV: ${DEPLOY_ENV}"
+echo "  AWS_PROFILE: ${AWS_PROFILE}"
+echo "  PROJECT: ${PROJECT}"
 
 # ──────────────── VERSION & IMAGE ────────────────
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
-VERSION=$(cat VERSION)
+echo "🆔 AWS Account ID: ${AWS_ACCOUNT_ID}"
+echo "📂 Infra DEPLOY_ENV: ${DEPLOY_ENV}"
+export VERSION=$(cat VERSION)
 
-echo "💥 Destroying infra version: ${VERSION}"
+echo "🚀 Destroying infra version: ${VERSION}"
 
 cdk context --clear
 cdk destroy \
   --require-approval never \
-  --context stage="${STAGE}" \
+  --context stage="${DEPLOY_ENV}" \
   --context version="${VERSION}"
-
-cd - >/dev/null
 
 echo "✅ Infra ${VERSION} destroyed successfully"
